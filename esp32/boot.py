ba3003d8e21e63f953f4469b39f50f1dfab9f717
Miles Opferkuch <miles.opferkuch123@gmail.com>
2021-12-07 21:52:44 -0800

    esp32: added comments, cleaned up code, added more error handling

--- esp32/boot.py
+++ esp32/boot.py
@@ -1,70 +1,93 @@
 import network
 import webrepl
 import machine
+import errno
 import time
 from time import localtime
 from ntptime import settime
 import socket
+import urequests
 
 '''
+Final project: WiFi smoke detector by Miles Opferkuch
+https://github.com/MilesOpferkuch/FireAlarm
+
 This script runs on the ESP32 microprocessor in my bedroom. It receives data
 from the Raspberry Pi in my garage, and sets off an alarm if it receives the signal to do so.
-Note that this uses Micropython and will only run on an ESP32.
+Note that this uses Micropython and will only run on an ESP32 (or possibly an ESP8266).
+It is also specific to how my circuit is wired.
 '''
 
-sta_if = network.WLAN(network.STA_IF)
 
-PIN_ALARM = machine.Pin(15, machine.Pin.OUT)                   # Pin to piezo alarm
+# Set up GPIO pins
+PIN_ALARM = machine.Pin(15, machine.Pin.OUT)                   # Pin to piezo buzzer
 PIN_LED_RED = machine.Pin(2, machine.Pin.OUT)                  # Pin to red status light
 PIN_LED_GREEN = machine.Pin(0, machine.Pin.OUT)                # Pin to green status light
-PIN_RST = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)  # Pin for reset button that turns off alarm for 5 minutes
+PIN_RST = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP)  # Pin to reset button
 
 # Pull low on startup
 PIN_ALARM.value(0)
 PIN_LED_RED.value(0)
 PIN_LED_GREEN.value(0)
 
-# Print message with UTC timestamp.
-# Micropython lacks a lot of the datetime formatting stuff from Python
-# so we have to do it manually.
-def print_t(msg):
-    time = localtime()  # "localtime" on MicroPython is actually UTC
-    timelist = []
-    # zero-padding
-    for i in time:
-        timelist.append(str(i))
-    for i in range (0, len(timelist)):
-        if len(timelist[i]) == 1:
-            timelist[i] = "0" + timelist[i]
+# The time we last received a ping from RPi.
+# If this exceeds 40 seconds, the connection is dead and we send a push notification.
+lastPingTime = 0.0
 
-    print(f'[{timelist[0]}-{timelist[1]}-{timelist[2]} {timelist[3]}:{timelist[4]}:{timelist[5]} UTC] {msg}')
+hasSentPushNotif = False
+
+# Get IFTTT API key
+iftttFile = open('ifttt.txt')
+IFTTT_KEY = iftttFile.read().strip()
+iftttFile.close()
+
+# Get WiFi credentials
+credfile = open('wifi.txt')
+SSID, PWD = str.splitlines(credfile.read())
+credfile.close()
+
+# Prints messages with a UTC timestamp. Micropython lacks a lot of
+# Python's datetime formatting stuff so we have to do it manually.
+def print_t(msg):
+    # localtime() returns a tuple so I convert it to a list for formatting.
+    # Despite the name, it's actually in UTC.
+    time = list(localtime())
+    # Zero-padding
+    for i in range (0, len(time)):
+        if len(time[i]) == 1:
+            time[i] = "0" + time[i]
+    # [yyyy-MM-dd HH:mm:ss UTC]
+    print(f'[{time[0]}-{time[1]}-{time[2]} {time[3]}:{time[4]}:{time[5]} UTC] {msg}')
+
+
+# Make error codes human-readable
+def errorName(e):
+    errorName = "???"
+    try:
+        errorName = errno.errorcode[e.errno]
+    except:
+        pass
+    return errorName
 
 
 # Connect to WiFi
-def do_connect():
-
-    credfile = open('wifi.txt')
-    wificreds = str.splitlines(credfile.read())
-    ssid, pwd = wificreds
-    credfile.close()
-
+def wifi_connect():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('Connecting to network %s...' % ssid)
+        print('Connecting to network %s...' % SSID)
         sta_if.active(True)
-        sta_if.connect(ssid, pwd)
+        sta_if.connect(SSID, PWD)
         while not sta_if.isconnected():
             PIN_LED_RED.value(1)
             PIN_LED_GREEN.value(1)
             pass
-    print('network config:', sta_if.ifconfig())
+    print('Connected to WiFi. Network config:', sta_if.ifconfig())
 
     # Set clock
     try:
         settime()
-    except OSError as e:
-        print("Error getting time from remote server:" + e)
-        print("Log timestamps will be incorrect.")
+    except OSError:
+        print("Error getting time from remote server. Log timestamps will be incorrect.")
     rtc = machine.RTC()
     print_t("System clock set.")
 
@@ -75,69 +98,105 @@ def do_connect():
         PIN_LED_GREEN.value(1)
         time.sleep(0.2)
         PIN_LED_GREEN.value(0)
-    
 
-do_connect()
+isConnectedToWifi = False
+try:
+    wifi_connect()
+    isConnectedToWifi = True
+except OSError as e:
+    PIN_LED_RED.value(1)
+    print(f"Could not connect to {SSID}: error code {str(e.errno)} [{errorName(e)}].\nTrying again in 5 seconds.")
+
 webrepl.start()
 
-
-def client(host, port):
-
-    print_t("Connecting to %s:%d..." % (host, port))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    
+# Connect to RPi and retry if it fails
+def server_connect(host, port):
+    isConnectedToPi = False
+    while not isConnectedToPi:
+        try:
+            print_t("Connecting to %s:%d..." % (host, port))
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((host, port))
+            isConnectedToPi = True
+        except OSError as e:
+            sock.close()
+            PIN_LED_RED.value(1)
+            print_t(f"Connection to RPi failed: error code {str(e.errno)} [{errorName(e)}].\nTrying again in 5 seconds.")
+            time.sleep(5)
     print_t("Connection established.")
-    # Connected to RPi server: solid green light and no red light 
     PIN_LED_RED.value(0)
     PIN_LED_GREEN.value(1)
+    return sock
+
+# Set up client
+def client(host, port):
 
     doAlarm = False
+    sock = server_connect(host, port)
 
-    # Callback for reset button
+    # Callback for reset button IRQ
     def rst_callback(pin):
         nonlocal doAlarm
         doAlarm = False
         PIN_LED_RED.value(0)
         PIN_ALARM.value(0)
-        print_t("Local alarm disabled.")
+        print_t("Reset button pressed. Local alarm turned off.")
 
-    # Set up cpu interrupt for reset button
+    # Set up CPU interrupt for reset button.
+    # This is on an interrupt so it works even while the thread is blocked.
     PIN_RST.irq(trigger=machine.Pin.IRQ_FALLING, handler=rst_callback)
 
+    # Main loop
     while True:
 
-        while doAlarm:
-            sock.settimeout(1)
-            # Listen for ALARMOFF signal from RPi
-            try:
-                print_t("Listening for data...")
-                data = sock.recv(512).decode().splitlines()
-                if "ALARMOFF" in data:
-                    doAlarm = False
-                    sock.settimeout(None)
-                    print_t("Alarm turned off by RPi")
-                    break
-            except:     # MicroPython doesn't like it if we catch socket.timeout specifically
-                pass
-            PIN_ALARM.value(1)
-            PIN_LED_RED.value(1)
-            time.sleep(1)
-            PIN_ALARM.value(0)
-            PIN_LED_RED.value(0)
-            time.sleep(0.5)
+        data = sock.recv(512).decode().splitlines()     # Listen for data from RPi
 
-        data = sock.recv(512).decode().splitlines()
-
+        # Fire the alarm if we get the signal from RPi
         if 'DOALARM' in data:
-            doAlarm = True
             print_t("Alarm triggered!")
+            sock.settimeout(1)
+            doAlarm = True
+            while doAlarm:
+                # Listen for ALARMOFF signal from RPi
+                try:
+                    print_t("Listening for data...")
+                    data = sock.recv(512).decode().splitlines()
+                    if "ALARMOFF" in data:
+                        doAlarm = False
+                        sock.settimeout(None)
+                        print_t("Alarm turned off by RPi")
+                        break
+                except:     # MicroPython doesn't like it if we catch socket.timeout specifically
+                    pass
+                # Blink LED and buzzer
+                PIN_ALARM.value(1)
+                PIN_LED_RED.value(1)
+                time.sleep(1)
+                PIN_ALARM.value(0)
+                PIN_LED_RED.value(0)
+                # No need to sleep here, the network timeout is 1 second
 
         # 'STATUS' should be sent by RPi every 30 seconds.
         # if esp32 fails to respond with 'CONNECTED', RPi sends a phone notification.
-        elif 'STATUS' in data:
+        if 'STATUS' in data:
+            lastPingTime = time.time()
+            hasSentPushNotif = False
             sock.send('CONNECTED'.encode())
             print_t("Status OK")
+            PIN_LED_GREEN.value(1)
+            PIN_LED_RED.value(0)
+
+        # Send notification if 40 seconds have passed since the last ping from RPi
+        if (time.time() > lastPingTime + 40 and not hasSentPushNotif):
+            PIN_LED_RED.value(1)
+            print_t("Lost connection to RPi! Sending push notification...")
+            notif = urequests.post('https://maker.ifttt.com/trigger/rpi_timeout/with/key/%s' % IFTTT_KEY)
+            notif.close()
+            print_t("Push notification sent.")
+            hasSentPushNotif = True
+
+            sock.close()    # Close the old socket
+            sock = server_connect(host, port)   # Try to make a new one
 
     sock.close()
     print_t("Client closed.")
@@ -148,8 +207,11 @@ def main():
     try:
         client('192.168.1.16', 5678)  # my RPi server is running on this IP
     except OSError as e:
-        print("Connection failed: %s" % e)
+        print(f"Connection to RPi failed: error code {str(e.errno)} [{errorName(e)}]")
         PIN_LED_RED.value(1)
+        PIN_LED_GREEN.value(0)
+    except KeyboardInterrupt:
+        PIN_LED_RED.value(0)
         PIN_LED_GREEN.value(0)
 
 main()
